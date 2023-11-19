53fa18216ed62683e452e951db026ee34b3784c7
wafna <wafna@duck.com>
2023-11-19 13:50:35 -0800

    flyway connects.

--- test/flyway/sql/V001_init.sql
+++ test/flyway/sql/V001_init.sql
@@ -1,20 +1,23 @@
- CREATE TABLE servers (
-   id INT NOT NULL uuid_generate_v4 (),
-   address VARCHAR(24) NOT NULL,
-   PRIMARY KEY (id)
+CREATE TABLE servers
+(
+    id      INT         NOT NULL uuid_generate_v4 (),
+    address VARCHAR(24) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE services (
-    id INT NOT NULL uuid_generate_v4 (),
-    user_id INT NOT NULL,
-    url VARCHAR(128) NOT NULL,
+CREATE TABLE services
+(
+    id      INT          NOT NULL uuid_generate_v4 (),
+    user_id INT          NOT NULL,
+    url     VARCHAR(128) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-CREATE TABLE trusts (
-    id INT NOT NULL uuid_generate_v4 (),
-    service_id INT NOT NULL,
+CREATE TABLE trusts
+(
+    id          INT NOT NULL uuid_generate_v4 (),
+    service_id  INT NOT NULL,
     consumer_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (consumer_id) REFERENCES users (id),
