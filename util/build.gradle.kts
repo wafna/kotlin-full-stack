580432c5d88273bf103e69b631c5c34a557fdbfa
Jason Feingold <jason.feingold@auditsight.com>
2024-02-26 15:06:32 -0800

    deps: commons-codec

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -9,5 +9,5 @@ dependencies {
     api("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     api("org.slf4j:slf4j-api:2.0.12")
     api("ch.qos.logback:logback-classic:1.5.0")
-    api("commons-codec:commons-codec:1.15")
+    api("commons-codec:commons-codec:1.16.1")
 }
