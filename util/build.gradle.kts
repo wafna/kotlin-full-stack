454a142f9cfc76a8a814eb2f05bea39053099b9a
Jason Feingold <jason.feingold@auditsight.com>
2024-02-26 11:18:10 -0800

    logback & slf4j

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -7,7 +7,7 @@ val kotlinVersion: String by project
 
 dependencies {
     api("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    api("org.slf4j:slf4j-api:2.0.7")
-    api("ch.qos.logback:logback-classic:1.4.7")
+    api("org.slf4j:slf4j-api:2.0.12")
+    api("ch.qos.logback:logback-classic:1.5.0")
     api("commons-codec:commons-codec:1.15")
 }
