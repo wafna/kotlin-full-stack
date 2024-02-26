8cf91d9e6e0a7f608e78969546b4b192277d1e9d
Jason Feingold <jason.feingold@auditsight.com>
2024-02-26 11:33:41 -0800

    deps: top level plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ group = "wafna"
 version = "0.0.1-SNAPSHOT"
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.46.0"
-    id("org.jetbrains.dokka") version "1.8.10"
-    id("io.gitlab.arturbosch.detekt") version "1.23.0-RC3"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.jetbrains.dokka") version "1.9.10"
+    id("io.gitlab.arturbosch.detekt") version "1.23.5"
 }
 
 repositories {
