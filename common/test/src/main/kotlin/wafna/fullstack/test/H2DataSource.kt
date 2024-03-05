1ce23e7c95bd490e71c722a52b5a45c67c310c20
wafna <wafna@duck.com>
2024-03-04 20:21:57 -0800

    tidy

--- common/test/src/main/kotlin/wafna/fullstack/test/H2DataSource.kt
+++ common/test/src/main/kotlin/wafna/fullstack/test/H2DataSource.kt
@@ -5,15 +5,11 @@ import com.zaxxer.hikari.HikariDataSource
 import javax.sql.DataSource
 
 fun withTestH2DataSource(borrow: (DataSource) -> Unit) {
-    HikariConfig()
-        .apply {
-            jdbcUrl = "jdbc:h2:mem:test-db;DB_CLOSE_DELAY=-1;MODE=PostgreSQL;DATABASE_TO_UPPER=false;"
-            username = "sa"
-            password = ""
-            driverClassName = org.h2.Driver::class.java.canonicalName
-        }.let {
-            HikariDataSource(it)
-        }.use {
-            borrow(it)
-        }
+    val config = HikariConfig().apply {
+        jdbcUrl = "jdbc:h2:mem:test-db;DB_CLOSE_DELAY=-1;MODE=PostgreSQL;DATABASE_TO_UPPER=false;"
+        username = "sa"
+        password = ""
+        driverClassName = org.h2.Driver::class.java.canonicalName
+    }
+    HikariDataSource(config).use(borrow)
 }
